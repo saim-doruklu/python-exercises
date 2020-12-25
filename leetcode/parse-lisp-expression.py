10deb458a2a4123d20cf0ede5cba6a3371e692f5
mevlut.saim.doruklu <mevlut.saim.doruklu@openjawtech.com>
2020-12-25 13:35:12 +0100

    working with better recursion

--- leetcode/parse-lisp-expression.py
+++ leetcode/parse-lisp-expression.py
@@ -1,83 +1,75 @@
 from __future__ import annotations
 import re
+
+
 # from typing import List, Tuple, Union
 
 class Expression:
 
-    def __init__(self, parent):
-        self.parent = parent
+    def __init__(self, parent_env):
         self.expressions = []
-        self.env = {}
+        self.env = {"0_parent_env":parent_env}
 
     def add_expression(self, expression):
         self.expressions.append(expression)
 
-    def find_in_env(self, symbol):
-        current_env = self
-        while current_env.env.get(symbol) is None:
-            current_env = current_env.parent
-        return current_env.env.get(symbol)
 
 
 class Solution:
     def evaluate(self, expression: str) -> int:
         tokens = re.findall(r"(\(|[-\w]+|\))", expression)
+        expression_depth = []
         current_expression = Expression(None)
         for token in tokens:
             if token == "(":
-                sub_expression = Expression(current_expression)
+                sub_expression = Expression(current_expression.env)
                 current_expression.add_expression(sub_expression)
+                expression_depth.append(current_expression)
                 current_expression = sub_expression
             elif token == ")":
-                current_expression = current_expression.parent
+                current_expression = expression_depth.pop()
             else:
                 current_expression.add_expression(token)
         return self.evaluate_in_env(current_expression.expressions[0])
 
-    def evaluate_in_env(self, expression):
-        expression_type = expression.expressions[0]
-
-        if expression_type == "add" or expression_type == "mult":
-            def function_add(x):
-                return x + cumulative_result
-
-            def function_mult(x):
-                return x * cumulative_result
-
-            if expression_type == "add":
-                cumulative_result = 0
-                current_function = function_add
+    def evaluate_in_env(self, expression, parent_env=None):
+        if type(expression) == str:
+            if self.is_number(expression):
+                return int(expression)
             else:
-                cumulative_result = 1
-                current_function = function_mult
+                return int(self.find_in_env(expression, parent_env))
 
+        expression_type = expression.expressions[0]
+        if expression_type == "add" or expression_type == "mult":
+            def calculate(fun_type, x, y):
+                if fun_type == "add":
+                    return x + (y or 0)
+                else:
+                    return x * (y or 1)
+
+            cumulative_result = None
             for sub_expression in expression.expressions[1:]:
-                evaluated_value = self.evaluate_value_or_expression(expression, sub_expression)
-                cumulative_result = current_function(evaluated_value)
+                evaluated_value = self.evaluate_in_env(sub_expression, expression.env)
+                cumulative_result = calculate(expression_type, evaluated_value, cumulative_result)
 
             return cumulative_result
         else:
-            last_expr_index = expression.expressions.__len__()
-            for index in range(1, last_expr_index, 2):
-                if index == last_expr_index - 1:
-                    return self.evaluate_value_or_expression(expression, expression.expressions[index])
+            last_expr_index = expression.expressions.__len__() - 1
+            for index in range(1, last_expr_index+1, 2):
+                if index == last_expr_index:
+                    return self.evaluate_in_env(expression.expressions[index], expression.env)
                 else:
                     symbol = expression.expressions[index]
-                    value = self.evaluate_value_or_expression(expression, expression.expressions[index+1])
+                    value = self.evaluate_in_env(expression.expressions[index + 1], expression.env)
                     expression.env[symbol] = value
-
-    def evaluate_value_or_expression(self, expression, sub_expression):
-        if type(sub_expression) == str:
-            if self.is_number(sub_expression):
-                evaluated_value = int(sub_expression)
-            else:
-                evaluated_value = int(expression.find_in_env(sub_expression))
-        else:
-            evaluated_value = self.evaluate_in_env(sub_expression)
-        return evaluated_value
 
     def is_number(self, string):
         return re.match(r"-?\d+", string)
+
+    def find_in_env(self, symbol, current_env):
+        while current_env.get(symbol) is None:
+            current_env = current_env["0_parent_env"]
+        return current_env.get(symbol)
 
 
 if __name__ == '__main__':
