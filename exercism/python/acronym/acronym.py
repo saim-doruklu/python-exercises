6f350a8faec11abc2ae710412cff3eaabce5b00b
mevlut.saim.doruklu <mevlut.saim.doruklu@openjawtech.com>
2020-12-29 23:00:22 +0100

    exercism (acronym, regex find first letter of words)

--- exercism/python/acronym/acronym.py
+++ exercism/python/acronym/acronym.py
@@ -2,4 +2,4 @@ import re
 
 
 def abbreviate(words: str):
-    return "".join([word.group()[0] for word in re.finditer("(([\w[^_]]+)('[\w[^_]]+)?)", words.upper())])
+    return "".join([word.group()[0] for word in re.finditer("(([a-zA-Z0-9]+)('[a-zA-Z0-9]+)?)", words.upper())])
