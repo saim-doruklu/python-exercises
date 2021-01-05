81ba9dd6353ec6048cb31dd2d720ca62a1a76bab
mevlut.saim.doruklu <mevlut.saim.doruklu@openjawtech.com>
2021-01-06 00:02:01 +0100

    exercism tournament(league table, string formatting)

--- exercism/python/tournament/tournament.py
+++ exercism/python/tournament/tournament.py
@@ -4,8 +4,8 @@ def tally(rows):
         tokens = row.split(";")
         first_team_name = tokens[0]
         second_team_name = tokens[1]
-        table.setdefault(first_team_name, {"name":first_team_name, "mp": 0, "w": 0, "d": 0, "l": 0, "p": 0})
-        table.setdefault(second_team_name, {"name":second_team_name, "mp": 0, "w": 0, "d": 0, "l": 0, "p": 0})
+        table.setdefault(first_team_name, {"name": first_team_name, "mp": 0, "w": 0, "d": 0, "l": 0, "p": 0})
+        table.setdefault(second_team_name, {"name": second_team_name, "mp": 0, "w": 0, "d": 0, "l": 0, "p": 0})
         first_team_info = table[first_team_name]
         second_team_info = table[second_team_name]
 
@@ -27,21 +27,16 @@ def tally(rows):
         first_team_info["mp"] += 1
         second_team_info["mp"] += 1
 
-
-    table_as_string_list = []
-
-    def add_row(name="Team", mp="MP", w="W", d="D", l="L", p="P"):
-        table_as_string_list.append("|".join([f'{name: <31}',
-                                              f'{mp: >3} ',
-                                              f'{w: >3} ',
-                                              f'{d: >3} ',
-                                              f'{l: >3} ',
-                                              f'{p: >3}']))
+    def to_string_repr(name, mp, w, d, l, p):
+        return "|".join([f'{name: <31}',
+                         f'{mp: >3} ',
+                         f'{w: >3} ',
+                         f'{d: >3} ',
+                         f'{l: >3} ',
+                         f'{p: >3}'])
 
     def sort_by(item):
         return -1 * item["p"], item["name"]
 
-    add_row()
-    for team in sorted(table.values(), key=sort_by):
-        add_row(**team)
-    return table_as_string_list
+    return [to_string_repr("Team", "MP", "W", "D", "L", "P")] + [to_string_repr(**team) for team in
+                                                                 sorted(table.values(), key=sort_by)]
