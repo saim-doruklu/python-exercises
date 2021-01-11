f0956c64cbebe3951782508625f14c192f3b3048
mevlut.saim.doruklu <mevlut.saim.doruklu@openjawtech.com>
2021-01-11 23:11:32 +0100

    2015 day one

---
+++ adventofcode/2015/one.py
@@ -0,0 +1,24 @@
+class DayOne:
+    def part_one(self, instructions):
+        return instructions.count("(") - instructions.count(")")
+
+    def part_two(self, instructions):
+        return len(list(self.convert_to_floor(instructions)))
+
+    def convert_to_floor(self, instructions):
+        total = 0
+        for char in list(instructions):
+            if total == -1:
+                break
+            instruction = 1 if char == "(" else -1
+            total += instruction
+            yield
+
+
+if __name__ == '__main__':
+    with open('one.txt', 'r') as file:
+        all_lines = file.readlines()
+        line_zero = all_lines[0]
+        day_one = DayOne()
+        print(day_one.part_one(line_zero))
+        print(day_one.part_two(line_zero))
---
+++ adventofcode/README.md
@@ -0,0 +1,1 @@
+https://github.com/narimiran/AdventOfCode2020
